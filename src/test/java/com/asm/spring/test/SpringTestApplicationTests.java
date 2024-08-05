f924b2992e8970be1ec63fd30421b1573111ebcf
asm <megait@gamil.com>
2024-08-05 21:54:29 +0900

    [안성민] - 최초 프로젝트 추가

---
+++ src/test/java/com/asm/spring/test/SpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asm.spring.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
