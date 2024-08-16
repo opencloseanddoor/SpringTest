5e9431be8f55921314652a261168f95a444ac820
asm <megait@gamil.com>
2024-08-16 21:51:12 +0900

    [안성민] - thymeleaf(변수, 조건문, 반복문) - 2024/08/16

---
+++ src/main/java/com/asm/spring/test/thymeleaf2/Thymeleaf2Controller.java
@@ -0,0 +1,35 @@
+package com.asm.spring.test.thymeleaf2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/test/thymeleaf")
+public class Thymeleaf2Controller 
+{
+	@GetMapping("/test01")
+	public String test01()
+	{
+		return "thymeleaf/test01";
+	}
+	
+	@GetMapping("/test02")
+	public String test02(Model model)
+	{
+		List<String> musicRanking = new ArrayList<>();
+		musicRanking.add("강남스타일");
+		musicRanking.add("벚꽃엔딩");
+		musicRanking.add("좋은날");
+		musicRanking.add("거짓말");
+		musicRanking.add("보고싶다");
+		
+		model.addAttribute("musicRanking", musicRanking);
+		
+		return "thymeleaf/test02";
+	}
+}
