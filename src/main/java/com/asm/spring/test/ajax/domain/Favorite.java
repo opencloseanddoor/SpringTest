f37a341284aadab7788b704f02f4fdae873bfde1
asm <megait@gamil.com>
2024-08-22 18:53:09 +0900

    [안성민] - 진행 중

---
+++ src/main/java/com/asm/spring/test/ajax/domain/Favorite.java
@@ -0,0 +1,44 @@
+package com.asm.spring.test.ajax.domain;
+
+import java.time.LocalDateTime;
+
+public class Favorite
+{
+	private int id;
+	private String name;
+	private String address;
+	private LocalDateTime createdAt;
+	private LocalDateTime updatedAt;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+}
